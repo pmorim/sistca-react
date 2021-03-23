db3e47a24587fb916890531482c74493bc94f0c4
Pedro Morim <1180798@isep.ipp.pt>
2021-03-23 22:42:58 +0000

    Added minimalistic example of class vs functional components

---
+++ examples/functional vs class/class.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Component extends React.Component {
+  // Component logic goes here...
+
+  render() {
+    return (
+      <p>
+        I am a <strong>class</strong> component.
+      </p>
+    );
+  }
+}
+
+export default Component;
---
+++ examples/functional vs class/functional.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Component() {
+  // Component logic goes here...
+
+  return (
+    <p>
+      I am a <strong>functional</strong> component.
+    </p>
+  );
+}
+
+export default Component;
