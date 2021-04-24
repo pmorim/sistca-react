817041cf3c60e086d81ae96719b393672e400f7e
Pedro Morim <1180798@isep.ipp.pt>
2021-04-24 16:03:27 +0100

    Turned posts into an array

--- examples/props/App.jsx
+++ examples/props/App.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import MyPost from "./MyPost";
 
 function App() {
-  const post1 = {
-    title: "Hello World!",
-    body: "I love to create React Apps.",
-  };
-  const post2 = {
-    title: "This is the second post",
-    body: "This is the body of the second post.",
-  };
+  const posts = [
+    {
+      title: "Hello World!",
+      body: "I love to create React Apps.",
+    },
+    {
+      title: "This is the second post",
+      body: "This is the body of the second post.",
+    }
+  ];
 
   return (
     <div className="App">
-      <MyPost data={post1} />
-      <MyPost data={post2} />
+      <MyPost data={posts[0]} />
+      <MyPost data={posts[1]} />
     </div>
   );
 }
