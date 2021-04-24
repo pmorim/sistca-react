d481601b9f3e640f295b504893d3f1bea828e38c
Pedro Morim <1180798@isep.ipp.pt>
2021-04-24 19:27:17 +0100

    Separate the posts
    Separated the posts into different variables to add emphasis on the next example.

--- examples/props/App.jsx
+++ examples/props/App.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import MyPost from "./MyPost";
 
 function App() {
-  const posts = [
-    {
+  const post1 = {
       title: "Hello World!",
       body: "I love to create React Apps.",
-    },
-    {
+  };
+  const post2 = {
       title: "This is the second post",
       body: "This is the body of the second post.",
-    }
-  ];
+  };
 
   return (
     <div className="App">
-      <MyPost data={posts[0]} />
-      <MyPost data={posts[1]} />
+      <MyPost data={post1} />
+      <MyPost data={post2} />
     </div>
   );
 }
